ce1573026935f5fb9ba36d449960469a56328b5f
Delta-47310 <482400473@alumnos.utzac.edu.mx>
2026-04-21 16:27:19 -0600

    feat: Improve PWA installability and service worker
    Update `manifest.json` to remove the `maskable` purpose from icons.
    Enhance the `firebase-messaging-sw.js` with a version comment.
    Modify `Navbar.tsx` to properly handle the `beforeinstallprompt` event and PWA installation by storing the `deferredPrompt` on the `window` object and clearing it upon successful installation.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,3 +1,4 @@
+// Service Worker Version: 1.1
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
 
